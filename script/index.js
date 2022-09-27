1f21d32fc6f403481140ec81de17597f36bfa022
Tobbie91 <oluwatobiakinrimisi@gmail.com>
2022-09-27 19:55:13 +0100

    final commit

--- script/index.js
+++ script/index.js
@@ -1,3 +1,42 @@
-function main() {}
+const main = () => {
+  const starWarsData = async () => {
+    try {
+      const response = await fetch("https://swapi.dev/api/people");
+      const data = await response.json();
+      const result = data.results;
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const displayCharData = async () => {
+    try {
+      const displayStarWarsData = await starWarsData();
 
-module.exports = { main }
+      let html = "";
+
+      displayStarWarsData.map((element, index) => {
+        let htmlEl = `    
+                  <div class="box">
+                      <img src=./img/image${index}.jpeg alt="star-wars"/>
+
+                          <h4 class="h4"> ${element.name}</h4>
+                          <details>
+                              <p>Gender: ${element.gender}</p>
+                              <p>Height: ${element.height}</p>
+                          </details>    
+                  </div>
+              `;
+        html += htmlEl;
+      });
+
+      let containerEl = document.getElementById("container");
+      containerEl.innerHTML = html;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  displayCharData();
+};
+main();
